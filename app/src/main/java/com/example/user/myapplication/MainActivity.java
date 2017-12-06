8f33ea11d5e5b21f406873bf2a45779cc6642c42
brad <brad@brad.tw>
2017-12-06 13:49:09 +0800

    201712061349

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +17,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void test1(View view){
+        new Thread(){
+            @Override
+            public void run() {
+                connectToLtu();
+            }
+        }.start();
+
+    }
+
+    private void connectToLtu(){
+
+        try {
+            // 1. URL : protocol:// Hostname or ip /.resources...
+            URL url = new URL("http://www.ltu.edu.tw/");
+            HttpURLConnection conn =  (HttpURLConnection)url.openConnection();
+            conn.connect();;
+            InputStream in =  conn.getInputStream();
+            int b;
+            while ((b = in.read()) != -1){
+                Log.v("brad", "" + (char)b);
+            }
+            in.close();
+            Log.v("brad", "OK");
+
+        }catch (Exception ee){
+            Log.v("brad", ee.toString());
+        }
+    }
+
+    public void test2(View view){
+
+    }
+    public void test3(View view){
+
+    }
+
 }
